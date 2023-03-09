a5ffa59b72b66f14e1f1eb0d40c9b979ed6f81c1
KathiWinter <kathi.winter@t-online.de>
2023-03-09 17:49:18 +0100

    evaluations script for CoinGame-2

---
+++ run_baselines.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# 1. Performance Evaluation
+
+python train.py CoinGame-2 LIO
+python train.py CoinGame-2 Gifting-ZEROSUM
+python train.py CoinGame-2 Gifting-BUDGET
+python train.py CoinGame-2 IAC
+
+python train.py CoinGame-2 LIO
+python train.py CoinGame-2 Gifting-ZEROSUM
+python train.py CoinGame-2 Gifting-BUDGET
+python train.py CoinGame-2 IAC
+
+python train.py CoinGame-2 LIO
+python train.py CoinGame-2 Gifting-ZEROSUM
+python train.py CoinGame-2 Gifting-BUDGET
+python train.py CoinGame-2 IAC
+
+python train.py CoinGame-2 LIO
+python train.py CoinGame-2 Gifting-ZEROSUM
+python train.py CoinGame-2 Gifting-BUDGET
+python train.py CoinGame-2 IAC
+
+python train.py CoinGame-2 LIO
+python train.py CoinGame-2 Gifting-ZEROSUM
+python train.py CoinGame-2 Gifting-BUDGET
+python train.py CoinGame-2 IAC
