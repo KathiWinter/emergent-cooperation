a7da7c31087f5df0cdfdce6ba33b202a5293830b
KathiWinter <kathi.winter@t-online.de>
2023-05-02 09:57:42 +0200

    batch job random

--- run_token_random.sh
+++ run_token_random.sh
@@ -2,9 +2,4 @@
 
 # 1. Performance Evaluation
 
-python train.py CoinGame-2 MATE-TD-RANDOM
-python train.py CoinGame-2 MATE-TD-RANDOM
-python train.py CoinGame-2 MATE-TD-RANDOM
-python train.py CoinGame-2 MATE-TD-RANDOM
-python train.py CoinGame-2 MATE-TD-RANDOM
-python train.py CoinGame-2 MATE-TD-RANDOM
+python train.py CoinGame-4 MATE-TD-RANDOM
