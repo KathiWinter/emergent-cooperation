e1aca96d481cc542101fc40124574eeef660b1a7
KathiWinter <kathi.winter@t-online.de>
2023-09-24 20:30:23 +0200

    batch scripts

---
+++ run-token-individual6.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+python train.py CoinGame-2 MATE-TD-INDIVIDUAL-6
+python train.py CoinGame-2 MATE-TD-INDIVIDUAL-6
---
+++ run-token-individual8.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+python train.py CoinGame-2 MATE-TD-INDIVIDUAL-8
+python train.py CoinGame-2 MATE-TD-INDIVIDUAL-8
