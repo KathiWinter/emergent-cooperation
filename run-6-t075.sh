d3e34f457dd78112844e1e6b14e5a503231a82b6
KathiWinter <kathi.winter@t-online.de>
2023-09-29 22:22:33 +0200

    batch scripts

---
+++ run-6-t075.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# 1. Performance Evaluation
+
+python train.py CoinGame-6 MATE-TD-T0.75
+python train.py CoinGame-6 MATE-TD-T0.75
+python train.py CoinGame-6 MATE-TD-T0.75
---
+++ run-6-t3.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# 1. Performance Evaluation
+
+python train.py CoinGame-6 MATE-TD-T3
+python train.py CoinGame-6 MATE-TD-T3
+python train.py CoinGame-6 MATE-TD-T3
