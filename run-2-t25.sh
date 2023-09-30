c668f76993bb66c185ddea163a3afdbebb1a3272
KathiWinter <kathi.winter@t-online.de>
2023-09-30 11:26:07 +0200

    batch scripts

---
+++ run-2-t25.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# 1. Performance Evaluation
+
+python train.py CoinGame-2x01 MATE-TD-T2.5
+python train.py CoinGame-2x01 MATE-TD-T2.5
+python train.py CoinGame-2x01 MATE-TD-T2.5
+python train.py CoinGame-2x01 MATE-TD-T2.5
+python train.py CoinGame-2x01 MATE-TD-T2.5
